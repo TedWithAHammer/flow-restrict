82ea48cc8dd06217c98136161ea32dce233cd66a
wangly1004@tpl.cntaiping.com <wangliyingwad@163.com>
2020-07-20 11:36:35 +0800

    init 限流算法实现

---
+++ src/main/java/com/leo/flowrestrict/FlowRestrictApplication.java
@@ -0,0 +1,13 @@
+package com.leo.flowrestrict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowRestrictApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowRestrictApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leo/flowrestrict/FlowRestrictApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leo.flowrestrict;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowRestrictApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
